f17bb185443d1f14a8e1847e5fa9973395ae33f6
justine92415 <justine92415@gmail.com>
2024-05-12 17:35:17 +0800

    (feat) 新增 studetn、teacher routes.ts

--- src/app/pages/student/student.routes.ts
+++ src/app/pages/student/student.routes.ts
@@ -1,47 +1,47 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-      {
-        path: '',
-        redirectTo: 'info',
-        pathMatch: 'full'
-      },
-      {
-        // 學生基本資料頁
-        path: 'info',
-        loadComponent: () =>
-          import('./student-info-page/student-info-page.component')
-      },
-      {
-        // 學生收藏課程頁
-        path: 'favorites',
-        loadComponent: () =>
-          import('./student-favorites-page/student-favorites-page.component')
-      },
-      {
-        // 學生行事曆頁
-        path: 'calendar',
-        loadComponent: () =>
-          import('./student-calendar-page/student-calendar-page.component')
-      },
-      {
-        // 學生已購買課程頁
-        path: 'courses',
-        loadComponent: () =>
-          import('./student-courses-page/student-courses-page.component')
-      },
-      {
-        // 學生交易紀錄頁
-        path: 'record',
-        loadComponent: () =>
-          import('./student-record-page/student-record-page.component')
-      },
-      {
-        // 學生喜好類別頁
-        path: 'preference',
-        loadComponent: () =>
-          import('./student-preference-page/student-preference-page.component')
-      }
+  {
+    path: '',
+    redirectTo: 'info',
+    pathMatch: 'full'
+  },
+  {
+    // 學生基本資料頁
+    path: 'info',
+    loadComponent: () =>
+      import('./student-info-page/student-info-page.component')
+  },
+  {
+    // 學生收藏課程頁
+    path: 'favorites',
+    loadComponent: () =>
+      import('./student-favorites-page/student-favorites-page.component')
+  },
+  {
+    // 學生行事曆頁
+    path: 'calendar',
+    loadComponent: () =>
+      import('./student-calendar-page/student-calendar-page.component')
+  },
+  {
+    // 學生已購買課程頁
+    path: 'courses',
+    loadComponent: () =>
+      import('./student-courses-page/student-courses-page.component')
+  },
+  {
+    // 學生交易紀錄頁
+    path: 'record',
+    loadComponent: () =>
+      import('./student-record-page/student-record-page.component')
+  },
+  {
+    // 學生喜好類別頁
+    path: 'preference',
+    loadComponent: () =>
+      import('./student-preference-page/student-preference-page.component')
+  }
 ];
 
 export default routes;
