ffc36e413cd64dbcfff75fe9fe957adcdf6a32d6
hedgehog-chien <j830813@gmail.com>
2024-05-23 00:00:25 +0800

    feature: home page 初始化 - home page 初始化 - 新增 angular material cdk

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
-export default class HomePageComponent {}
+export default class HomePageComponent {
+
+}
