5efa17aec6c448a03e1675a36a48a818eaf22827
justine92415 <107650092+justine92415@users.noreply.github.com>
2024-05-29 21:49:46 +0800

    (feat) 後台管理頁共用版型切版 (#13)
    - 新增 layout2 路由
    - 移動 student、teacher 路由
    - 新增 預約日期選擇器、課程詳情頁日曆 template （未完成）

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
+import { MenuService } from './shared/services/menu.service';
 import { Component, OnInit, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { AuthStatusService } from './shared/services/authStatus.service';
+import { filter } from 'rxjs';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,8 +12,22 @@ import { AuthStatusService } from './shared/services/authStatus.service';
 })
 export class AppComponent implements OnInit {
   private authStatusService = inject(AuthStatusService);
+  private menuService = inject(MenuService);
+  private router = inject(Router);
 
   ngOnInit(): void {
     this.authStatusService.checkLoginStatus();
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          const url = event.url;
+          if (url.startsWith('/layout2/student')) {
+            this.menuService.updateMenuList('student');
+          } else if (url.startsWith('/layout2/teacher')) {
+            this.menuService.updateMenuList('teacher');
+          }
+        }
+      });
   }
 }
