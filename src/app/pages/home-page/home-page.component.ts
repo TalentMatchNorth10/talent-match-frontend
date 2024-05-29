b3ef58ed6f1ee055a6318617da934fbec07134a8
Jean Chien <jean.chien@caloudi.com>
2024-05-29 13:55:18 +0800

    feat: 課程卡片
    - 課程卡片可以導航至課程頁面

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -155,93 +155,8 @@ export default class HomePageComponent implements OnInit {
   }
 }
 
-const FakeVideos: VideoCardData[] = [
+const FakeCardData = [
   {
-    video_id: '1',
-    name: 'Introduction to JavaScript',
-    category: 'Programming',
-    intro: 'Learn the basics of JavaScript programming language.',
-    video_type: 'youtube',
-    url: 'https://youtu.be/bjCjJW_aAUk?si=zO58J-33XKrAGHjR',
-    teacher_id: 't-1',
-    course_id: 'c-1'
-  },
-  {
-    video_id: '2',
-    name: 'Cooking Italian Pasta',
-    category: 'Cooking',
-    intro: 'Master the art of making authentic Italian pasta dishes.',
-    video_type: 'storage',
-    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
-    teacher_id: 't-2',
-    course_id: 'c-2'
-  },
-  {
-    video_id: '3',
-    name: 'Yoga for Beginners',
-    category: 'Fitness',
-    intro: 'Start your yoga journey with these beginner-friendly poses.',
-    video_type: 'storage',
-    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
-    teacher_id: 't-3',
-    course_id: 'c-3'
-  },
-  {
-    video_id: '4',
-    name: 'Introduction to Astrophysics',
-    category: 'Science',
-    intro:
-      'Explore the wonders of the universe with basic astrophysics concepts.',
-    video_type: 'storage',
-    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
-    teacher_id: 't-4',
-    course_id: 'c-4'
-  },
-  {
-    video_id: '5',
-    name: 'Abstract Oil Painting Techniques',
-    category: 'Art',
-    intro: 'Discover unique techniques for creating abstract oil paintings.',
-    video_type: 'storage',
-    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
-    teacher_id: 't-5',
-    course_id: 'c-5'
-  },
-  {
-    video_id: '6',
-    name: 'Healthy Smoothie Recipes',
-    category: 'Cooking',
-    intro: 'Learn how to make delicious and nutritious smoothies at home.',
-    video_type: 'storage',
-    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
-    teacher_id: 't-6',
-    course_id: 'c-6'
-  },
-  {
-    video_id: '7',
-    name: 'Introduction to Machine Learning',
-    category: 'Technology',
-    intro: 'Get started with machine learning algorithms and applications.',
-    video_type: 'storage',
-    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4',
-    teacher_id: 't-7',
-    course_id: 'c-7'
-  },
-  {
-    video_id: '8',
-    name: "Beginner's Guide to Meditation",
-    category: 'Wellness',
-    intro: 'Start your meditation practice with these simple techniques.',
-    video_type: 'storage',
-    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4',
-    teacher_id: 't-8',
-    course_id: 'c-8'
-  }
-];
-
-const FakeCardData: CardData[] = [
-  {
-    course_id: '1',
     mainImg: 'https://fakeimg.pl/300/',
     title: '課程名稱1',
     content: '課程內容1',
@@ -255,7 +170,6 @@ const FakeCardData: CardData[] = [
     ratingCount: 100
   },
   {
-    course_id: '2',
     mainImg: 'https://fakeimg.pl/300/',
     title: '課程名稱2',
     content: '課程內容2',
@@ -269,7 +183,6 @@ const FakeCardData: CardData[] = [
     ratingCount: 100
   },
   {
-    course_id: '3',
     mainImg: 'https://fakeimg.pl/300/',
     title: '課程名稱2',
     content:
@@ -284,7 +197,6 @@ const FakeCardData: CardData[] = [
     ratingCount: 100
   },
   {
-    course_id: '4',
     mainImg: 'https://fakeimg.pl/300/',
     title: '課程名稱2',
     content: '課程內容2',
@@ -298,7 +210,6 @@ const FakeCardData: CardData[] = [
     ratingCount: 100
   },
   {
-    course_id: '5',
     mainImg: 'https://fakeimg.pl/300/',
     title: '課程名稱2',
     content: '課程內容2',
