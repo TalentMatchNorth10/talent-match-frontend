7ddfcff6c6c4eec351b9f7a7d265e2b65fdc15b2
th1230 <th1230@users.noreply.github.com>
2024-06-01 10:00:01 +0000

    deploy: 54bde7ef9d13da2c6a4a9d7f835d6da7ace7d85e

---
+++ chunk-IKK3L6HZ.js
@@ -0,0 +1,1 @@
+import{Eb as p,aa as n,jb as o,kb as r,zb as i}from"./chunk-OQO4KDA7.js";import"./chunk-2XJVAMHT.js";var f=(()=>{let e=class e{};e.\u0275fac=function(a){return new(a||e)},e.\u0275cmp=n({type:e,selectors:[["app-chart-page"]],standalone:!0,features:[p],decls:2,vars:0,template:function(a,c){a&1&&(o(0,"p"),i(1,"chart-page works!"),r())}});let t=e;return t})();export{f as default};
