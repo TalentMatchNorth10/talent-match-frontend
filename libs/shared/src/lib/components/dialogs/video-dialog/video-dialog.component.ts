960b29c4323242786d0c382d30d8887e0269b0f3
hedgehog-chien <j830813@gmail.com>
2024-06-07 00:38:47 +0800

    refactor: 首頁 - 影音卡片大小固定(參考figma) - 課程卡片大小固定(參考figma) - 評論卡片大小固定(參考figma) - 評論區布局參考figma - 搜尋input改為共用組件

--- libs/shared/src/lib/components/dialogs/video-dialog/video-dialog.component.ts
+++ libs/shared/src/lib/components/dialogs/video-dialog/video-dialog.component.ts
@@ -7,6 +7,8 @@ import {
   Inject
 } from '@angular/core';
 import { DialogRef, DIALOG_DATA } from '@angular/cdk/dialog';
+import { VideoCardData } from '../../short-video-card/video-card.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'tmf-video-dialog',
@@ -20,12 +22,14 @@ export class VideoDialogComponent {
   isPlaying: WritableSignal<boolean> = signal(false);
   progress: number = 0;
 
-  videoUrl =
-    'https://storage.googleapis.com/talent-match-fd353.appspot.com/images/users/2f8dda8c-fcc9-4a19-bb2a-000bf1833c78.mp4?GoogleAccessId=firebase-adminsdk-vnucd%40talent-match-fd353.iam.gserviceaccount.com&Expires=16756646400&Signature=i83ZRQbK8PoBms7MLXx1TOcmNJaxl86lMpuxSkDiajDnu%2FRDY6V8U2eqOZXSVF6QlSYC0cpxzWWP0emSNa477pMLW5KZNfB6BfX2vf2o%2F7%2B%2FKhYIieT6MGzc%2F3C6uQKRhePtq9RJQEpePCJTkpA3WbVs9lrn08qj6PDB15qmZMpP37ryLODAZrkQTnOhbq1aNdyC3P4WrxIAPdX3oe698yzpwfqG%2Bh49NAO54u%2BaHaUuY5U24DF2bD6ZQ6Yg%2B53EfB6vPgPWI9pjPvqFQc7SFfYon9%2BQFzG3C6sSbMWZOWLKrkzXyzrxPlyoPkjKn3ITl3MVwAaaTG8oZT6cqBGcrg%3D%3D';
+
+  // videoUrl =
+  //   'https://storage.googleapis.com/talent-match-fd353.appspot.com/images/users/2f8dda8c-fcc9-4a19-bb2a-000bf1833c78.mp4?GoogleAccessId=firebase-adminsdk-vnucd%40talent-match-fd353.iam.gserviceaccount.com&Expires=16756646400&Signature=i83ZRQbK8PoBms7MLXx1TOcmNJaxl86lMpuxSkDiajDnu%2FRDY6V8U2eqOZXSVF6QlSYC0cpxzWWP0emSNa477pMLW5KZNfB6BfX2vf2o%2F7%2B%2FKhYIieT6MGzc%2F3C6uQKRhePtq9RJQEpePCJTkpA3WbVs9lrn08qj6PDB15qmZMpP37ryLODAZrkQTnOhbq1aNdyC3P4WrxIAPdX3oe698yzpwfqG%2Bh49NAO54u%2BaHaUuY5U24DF2bD6ZQ6Yg%2B53EfB6vPgPWI9pjPvqFQc7SFfYon9%2BQFzG3C6sSbMWZOWLKrkzXyzrxPlyoPkjKn3ITl3MVwAaaTG8oZT6cqBGcrg%3D%3D';
 
   constructor(
     public dialogRef: DialogRef<boolean, VideoDialogComponent>,
-    @Inject(DIALOG_DATA) public data: any
+    protected readonly router: Router,
+    @Inject(DIALOG_DATA) public data: VideoCardData
   ) {}
 
   ngAfterViewInit(): void {
@@ -52,5 +56,11 @@ export class VideoDialogComponent {
 
   close() {
     this.dialogRef.close();
+  }
+
+  public navigateToCoursePage(data: VideoCardData): void {
+    // console.log(data.course_id)
+    this.router.navigate([`course-detail/${data.course_id}`]);
+    this.close();
   }
 }
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -4,6 +4,8 @@ import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
 import { CardComponent } from '@tmf/libs-shared/components/card/card.component';
 import { CardData } from '@tmf/libs-shared/components/card/card.interface';
 import {
+  InputComponent,
+  InputType,
   OptionComponent,
   ReviewCardComponent,
   SelectComponent,
@@ -13,25 +15,31 @@ import { VideoCardData } from '@tmf/libs-shared/components/short-video-card/vide
 import { StarRatingComponent } from '@tmf/libs-shared/components/star-rating/star-rating.component';
 import { ReviewData } from '@tmf/libs-shared/components/review-card/review.interface';
 import SwiperCore from 'swiper';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { VideoCardComponent } from '@tmf/libs-shared/components/video-card/video-card.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ShortVideoCardComponent,
     CardComponent,
     StarRatingComponent,
     ReviewCardComponent,
     SelectComponent,
-    OptionComponent
+    OptionComponent,
+    InputComponent,
+    VideoCardComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export default class HomePageComponent implements OnInit {
+  readonly InputType = InputType;
+
   public shortsSubjectOptions: string[] = [
     '全部',
     '烹飪料理',
@@ -58,7 +66,7 @@ export default class HomePageComponent implements OnInit {
 
   public currentWindowSize: string = '';
 
-  control = new FormControl(false, [Validators.required]);
+  control = new FormControl('準備好啟程了嗎？', [Validators.required]);
 
   items = [
     { label: '依關鍵字', value: 1 },
@@ -105,6 +113,9 @@ export default class HomePageComponent implements OnInit {
     console.log(this.currentWindowSize);
   }
 
+  search(event: any) {
+    console.log(event);
+  }
   public shortsPrev() {
     if (this.shortsCurrentIndex > 0) {
       this.shortsCurrentIndex--;
@@ -137,7 +148,92 @@ export default class HomePageComponent implements OnInit {
 }
 
 const FakeVideos: VideoCardData[] = [
+  // {
+  //   video_id: '1',
+  //   name: 'Introduction to JavaScript',
+  //   category: 'Programming',
+  //   intro: 'Learn the basics of JavaScript programming language.',
+  //   video_type: 'youtube',
+  //   url: 'https://youtu.be/bjCjJW_aAUk?si=zO58J-33XKrAGHjR',
+  //   teacher_id: 't-1',
+  //   course_id: 'c-1'
+  // },
   {
+    video_id: '2',
+    name: 'Cooking Italian Pasta',
+    category: 'Cooking',
+    intro: 'Master the art of making authentic Italian pasta dishes.',
+    video_type: 'storage',
+    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+    teacher_id: 't-2',
+    course_id: 'c-2'
+  },
+  {
+    video_id: '3',
+    name: 'Yoga for Beginners',
+    category: 'Fitness',
+    intro: 'Start your yoga journey with these beginner-friendly poses.',
+    video_type: 'storage',
+    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
+    teacher_id: 't-3',
+    course_id: 'c-3'
+  },
+  {
+    video_id: '4',
+    name: 'Introduction to Astrophysics',
+    category: 'Science',
+    intro:
+      'Explore the wonders of the universe with basic astrophysics concepts.',
+    video_type: 'storage',
+    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
+    teacher_id: 't-4',
+    course_id: 'c-4'
+  },
+  {
+    video_id: '5',
+    name: 'Abstract Oil Painting Techniques',
+    category: 'Art',
+    intro: 'Discover unique techniques for creating abstract oil paintings.',
+    video_type: 'storage',
+    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
+    teacher_id: 't-5',
+    course_id: 'c-5'
+  },
+  {
+    video_id: '6',
+    name: 'Healthy Smoothie Recipes',
+    category: 'Cooking',
+    intro: 'Learn how to make delicious and nutritious smoothies at home.',
+    video_type: 'storage',
+    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
+    teacher_id: 't-6',
+    course_id: 'c-6'
+  },
+  {
+    video_id: '7',
+    name: 'Introduction to Machine Learning',
+    category: 'Technology',
+    intro: 'Get started with machine learning algorithms and applications.',
+    video_type: 'storage',
+    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4',
+    teacher_id: 't-7',
+    course_id: 'c-7'
+  },
+  {
+    video_id: '8',
+    name: "Beginner's Guide to Meditation",
+    category: 'Wellness',
+    intro: 'Start your meditation practice with these simple techniques.',
+    video_type: 'storage',
+    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4',
+    teacher_id: 't-8',
+    course_id: 'c-8'
+  }
+];
+
+const FakeCardData: CardData[] = [
+  {
+    course_id: '1',
     mainImg: 'https://fakeimg.pl/300/',
     title: '課程名稱1',
     content: '課程內容1',
@@ -247,19 +343,19 @@ export const FakeReviewData = [
     rate: 4.9,
     comment: 'Left order talk management condition receive.',
     create_datetime: new Date('2024-04-06T23:52:02')
-  },
-  {
-    nick_name: 'scott59',
-    avator_image: 'https://dummyimage.com/636x651',
-    rate: 4.7,
-    comment: 'Hundred want room begin box raise.',
-    create_datetime: new Date('2024-04-16T23:52:28')
-  },
-  {
-    nick_name: 'martinezlaura',
-    avator_image: 'https://dummyimage.com/636x651',
-    rate: 4.8,
-    comment: 'Option price suddenly.',
-    create_datetime: new Date('2024-01-15T20:49:07')
   }
+  // {
+  //   nick_name: 'scott59',
+  //   avator_image: 'https://dummyimage.com/636x651',
+  //   rate: 4.7,
+  //   comment: 'Hundred want room begin box raise.',
+  //   create_datetime: new Date('2024-04-16T23:52:28')
+  // },
+  // {
+  //   nick_name: 'martinezlaura',
+  //   avator_image: 'https://dummyimage.com/636x651',
+  //   rate: 4.8,
+  //   comment: 'Option price suddenly.',
+  //   create_datetime: new Date('2024-01-15T20:49:07')
+  // }
 ];
