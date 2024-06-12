cec0806e5663e8e371fbc9b33d11ca394134e523
hedgehog-chien <j830813@gmail.com>
2024-06-12 23:12:07 +0800

    feat: 教師詳細 - 教師詳細頁面

--- src/app/pages/teacher/teacher-detail-page/teacher-detail-page.component.ts
+++ src/app/pages/teacher/teacher-detail-page/teacher-detail-page.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, NgClass } from '@angular/common';
-import { Component, WritableSignal, signal } from '@angular/core';
+import { Component, OnInit, WritableSignal, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CardComponent } from '@tmf/libs-shared/components/card/card.component';
 import { VideoCardData } from '@tmf/libs-shared/components/short-video-card/video-card.interface';
@@ -7,6 +7,7 @@ import { StarRatingComponent } from '@tmf/libs-shared/components/star-rating/sta
 import { VideoCardComponent } from '@tmf/libs-shared/components/video-card/video-card.component';
 import { WeeklyCalendarComponent } from '@tmf/libs-shared/components/weekly-calendar/weekly-calendar.component';
 import { CourseData, FakeVideos } from '../../course-detail-page/mock-data';
+import { CardData } from '@tmf/libs-shared/components/card/card.interface';
 
 @Component({
   selector: 'app-teacher-detail-page',
@@ -23,9 +24,18 @@ import { CourseData, FakeVideos } from '../../course-detail-page/mock-data';
   templateUrl: './teacher-detail-page.component.html',
   styleUrl: './teacher-detail-page.component.scss'
 })
-export default class TeacherDetailPageComponent {
+export default class TeacherDetailPageComponent implements OnInit {
   // activeSection: WritableSignal<string> = signal('sectionA');
 
-  videoData: VideoCardData[] = FakeVideos;
-  courseData = CourseData;
+  introductionData!: VideoCardData;
+  advantagesData!: VideoCardData;
+
+  recommendedCourseData!:CardData[]
+
+  ngOnInit(): void {
+    this.introductionData = FakeVideos[0]
+    this.advantagesData = FakeVideos[1]
+
+    this.recommendedCourseData = CourseData
+  }
 }
