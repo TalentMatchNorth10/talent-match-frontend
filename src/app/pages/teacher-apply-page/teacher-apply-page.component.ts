949b360d014d08effdb72e218335c88f1edaf2cf
hedgehog-chien <j830813@gmail.com>
2024-06-19 00:09:15 +0800

    fear: teacher-apply-stepper

--- src/app/pages/teacher-apply-page/teacher-apply-page.component.ts
+++ src/app/pages/teacher-apply-page/teacher-apply-page.component.ts
@@ -1,10 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-teacher-apply-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './teacher-apply-page.component.html',
   styleUrl: './teacher-apply-page.component.scss'
 })
-export default class TeacherApplyPageComponent {}
+export default class TeacherApplyPageComponent {
+  steps = [
+    { name: '填寫基本資料', completed: false },
+    { name: '填寫履歷', completed: false },
+    { name: '填寫學歷背景', completed: false },
+    { name: '填寫教學證照', completed: false }
+  ];
+
+  currentStepIndex = 0;
+
+  nextStep() {
+    if (this.currentStepIndex < this.steps.length - 1) {
+      this.currentStepIndex++;
+    }
+  }
+
+  prevStep() {
+    if (this.currentStepIndex > 0) {
+      this.currentStepIndex--;
+    }
+  }
+
+  completeStep(index: number) {
+    this.steps[index].completed = true;
+  }
+}
