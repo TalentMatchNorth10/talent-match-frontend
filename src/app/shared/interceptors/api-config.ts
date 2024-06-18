f3e8214872ce5d39b530b0754b80be64b1759141
thomasYeh <thomasyeayea@gmail.com>
2024-06-18 23:03:44 +0800

    (fix) 修正購物車錯誤
    - 修正購買完成畫面邏輯
    - header購物車選單增加跳轉至詳細頁功能
    - api-config加入order api保護
    - select component修正選單顯示邏輯

--- src/app/shared/interceptors/api-config.ts
+++ src/app/shared/interceptors/api-config.ts
@@ -1,6 +1,7 @@
 export const PROTECTED_PATHS = [
   { pattern: '/api/auth/refresh', methods: ['POST'] },
   { pattern: '/api/shop/cart', methods: ['GET', 'DELETE'] },
+  { pattern: '/api/shop/order', methods: ['GET'] },
   { pattern: '/api/student/purchased_courses', methods: ['GET'] },
   { pattern: '/api/student_reservation/reserve_course', methods: ['POST'] },
   { pattern: '/api/student_reservation/reserves_time', methods: ['POST'] }
