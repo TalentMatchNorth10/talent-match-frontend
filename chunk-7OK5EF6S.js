f9a5a6b0e01e88fa83d8abf19d962e690aa1c898
justine92415 <justine92415@users.noreply.github.com>
2024-06-23 13:27:17 +0000

    deploy: 49395241ed270418099ae363c1b49f908da6b03f

--- chunk-7OK5EF6S.js
+++
@@ -1,1 +0,0 @@
-import{Gb as p,da as n,gb as o,hb as r,yb as i}from"./chunk-MURYLCTF.js";import"./chunk-TMC7WMLO.js";var f=(()=>{let e=class e{};e.\u0275fac=function(a){return new(a||e)},e.\u0275cmp=n({type:e,selectors:[["app-chart-page"]],standalone:!0,features:[p],decls:2,vars:0,template:function(a,c){a&1&&(o(0,"p"),i(1,"chart-page works!"),r())}});let t=e;return t})();export{f as default};
