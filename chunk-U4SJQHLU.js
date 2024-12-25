5548b7b758c729b4c335abb29aa4401b0980c23d
th1230 <th1230@users.noreply.github.com>
2024-12-25 14:58:24 +0000

    deploy: 964a636a4d5490f06794d98554900ab9f94ef760

---
+++ chunk-U4SJQHLU.js
@@ -0,0 +1,1 @@
+var t={production:!0,basePath:"https://talent-match-backend.onrender.com",apiUrl:"https://talent-match-backend.onrender.com",webSocketPath:"wss://talent-match-backend.onrender.com"};export{t as a};
--- chunk-XIR4PVUX.js
+++
@@ -1,1 +0,0 @@
-var t={production:!0,basePath:"https://talent-match-backend.onrender.com",apiUrl:"https://talent-match-backend.onrender.com"};export{t as a};
